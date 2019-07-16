516bd050963d8d4745487bd492fc75936071e0c5
Slav Donchev <35224472+slavrd@users.noreply.github.com>
2019-07-16 10:30:49 +0300

    Update switch-nginx-traffic.sh

--- ops/scripts/switch-nginx-traffic.sh
+++ ops/scripts/switch-nginx-traffic.sh
@@ -9,7 +9,7 @@ NGINX_CONFIG_PATH="/etc/nginx/sites-available/default"
 # replace config and restart nginx
 if [ -f $CONFIG_STORE_PATH/$INPUT ]; then
     sudo cp $CONFIG_STORE_PATH/$INPUT $NGINX_CONFIG_PATH
-    sudo service nginx restart
+    sudo service nginx reload
 else
     echo "configuration: $CONFIG_STORE_PATH/$INPUT does not exist."
 fi
